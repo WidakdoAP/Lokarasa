2bed2a9173c1b72527fd54206d57c93bfa9ef262
WidakdoAP <wapofficial2629@gmail.com>
2026-08-03 20:16:32 +0700

    feat: added website logo

---
+++ resources/js/components/header/Logo.tsx
@@ -0,0 +1,11 @@
+import { Link } from "@inertiajs/react";
+
+export default function Logo() {
+  return (
+      <div>
+          <Link href="/market">
+              <img src="/images/Lokarasa.svg" alt="Lokarasa" />
+          </Link>
+      </div>
+  );
+}
