c295026e127cd40dab0253d49f9cce6397d33e83
WidakdoAP <wapofficial2629@gmail.com>
2026-08-03 20:45:00 +0700

    feat: added new route to the market

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
-Route::inertia('/', 'welcome')->name('home');
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::inertia('dashboard', 'dashboard')->name('dashboard');
-});
-
-require __DIR__.'/settings.php';
+Route::get('/', fn() => Inertia::render('welcome'));
+Route::get('/market', [ProductController::class, 'index']);
