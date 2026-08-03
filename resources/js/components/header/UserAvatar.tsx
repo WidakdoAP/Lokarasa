439fa2e0f04e83769b6d7a0e6ca6e67ba04590de
WidakdoAP <wapofficial2629@gmail.com>
2026-08-03 20:14:57 +0700

    feat: added user avatar

---
+++ resources/js/components/header/UserAvatar.tsx
@@ -0,0 +1,20 @@
+import { User } from "lucide-react";
+import {
+    Avatar,
+    AvatarFallback,
+    AvatarImage,
+} from "@/components/ui/avatar";
+
+export default function UserAvatar() {
+  return (
+      <Avatar className="border">
+          <AvatarImage
+              src=""
+              alt="User"
+          />
+          <AvatarFallback>
+              <User size="icon" />
+          </AvatarFallback>
+      </Avatar>
+  );
+}
