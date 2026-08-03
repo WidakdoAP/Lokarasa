c5821b37cf338d5e73500bf3880b860cca0b729c
WidakdoAP <wapofficial2629@gmail.com>
2026-08-03 19:55:48 +0700

    feat: added product model

---
+++ app/Models/Product.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property int $id
+ * @property string $name
+ * @property string $description
+ * @property string $price
+ * @property string|null $image
+ */
+class Product extends Model {
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'price',
+        'description',
+        'image',
+    ];
+}
