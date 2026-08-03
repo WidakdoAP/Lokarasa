d89410817b0c46095a3b62d5f73d66fa2bab8091
WidakdoAP <wapofficial2629@gmail.com>
2026-08-03 20:09:52 +0700

    feat: added seeder for table products

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,24 +2,19 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Product;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
+class DatabaseSeeder extends Seeder {
     use WithoutModelEvents;
 
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+    public function run(): void {
+        Product::factory()->create([
+            'name' => 'Risoles',
+            'price' => 18000,
+            'description' => 'Risoles mayo dengan isi sosis & telur.',
         ]);
+        Product::factory()->count(5)->create();
     }
 }
