20f429f746d38c8dccab82c29ec973845dab18b3
WidakdoAP <wapofficial2629@gmail.com>
2026-08-03 19:54:07 +0700

    feat: added products controller

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class ProductController extends Controller
+{
+    public function index(): Response
+    {
+        $products = Product::latest()->get();
+
+        return Inertia::render('Market', [
+            'products' => $products->map(fn (Product $product) => [
+                'id' => $product->id,
+                'name' => $product->name,
+                'description' => $product->description,
+                'price' => $product->price,
+                'image' => filled($product->image) ? asset('storage/' . $product->image) : null,
+            ]),
+        ]);
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        $path = $request->file('image')->store('products', 'public');
+        Product::create([
+            'name' => $request->name,
+            'description' => $request->description,
+            'price' => $request->price,
+            'image' => $path,
+        ]);
+        return redirect()->route('market');
+    }
+}
